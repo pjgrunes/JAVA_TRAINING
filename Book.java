9a828f4f771aaf0742be2371ac7a5b29fa9ec1fa
Pia Joy G. Runes <pjgrunes@headoffice.corp.bpi.com.ph>
2025-12-04 14:15:24 +0800

    Activity 6

---
+++ Book.java
@@ -0,0 +1,13 @@
+package ph.com.bpi.hello;
+
+import ph.com.bpi.hello.M2_ACT1.*;
+	
+	public class Book{
+		String title;
+		
+		public void read() {
+			System.out.println("Title: "+ title);
+		}
+
+	}
+
---
+++ M2_ACT1.java
@@ -0,0 +1,16 @@
+package ph.com.bpi.hello;
+
+public class M2_ACT1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Book mybook = new Book();
+			mybook.title = "Snow white";
+			mybook.read();
+			
+			System.out.println();
+					
+
+	}
+
+}
