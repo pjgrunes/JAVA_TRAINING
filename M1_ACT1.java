ac96f500075d111244861d2cc14a6a7498abf365
Pia Joy G. Runes <pjgrunes@headoffice.corp.bpi.com.ph>
2025-12-04 11:26:15 +0800

    activity 1

---
+++ M1_ACT1.java
@@ -0,0 +1,16 @@
+package ph.com.bpi.hello;
+import java.util.*;
+public class M1_ACT1 {
+ 
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+        System.out.println("what is your name? ");
+        String name = input.nextLine();
+
+        System.out.println("Your name is " + name);
+
+	}
+
+}
