21668b4b9955a765c4872ef5f19830e86ca931b9
Pia Joy G. Runes <pjgrunes@headoffice.corp.bpi.com.ph>
2025-12-04 16:16:19 +0800

    M2 Activity 2

---
+++ M2_ACT2.java
@@ -0,0 +1,10 @@
+package ph.com.bpi.hello;
+public class M2_ACT2 {
+    public static void main(String[] args) {
+        WhiteBoard wb = new WhiteBoard();
+        int result = wb.addNumbers(5, 3);
+        System.out.println("The total is: " + String.valueOf(result));
+    }
+}
+
+
