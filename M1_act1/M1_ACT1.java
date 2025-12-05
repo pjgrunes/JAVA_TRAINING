8feb918fefd9591bba1c35e59c7eb634bda23f87
Pia Joy G. Runes <pjgrunes@headoffice.corp.bpi.com.ph>
2025-12-05 18:51:26 +0800

    correct

---
+++ M1_act1/M1_ACT1.java
@@ -0,0 +1,16 @@
+package module1_Activity1;
+import java.util.*;
+public class M1_ACT1 {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+        System.out.println("what is your name? ");
+        String name = input.nextLine();
+
+        System.out.println("Your name is " + name);
+
+	}
+
+}
