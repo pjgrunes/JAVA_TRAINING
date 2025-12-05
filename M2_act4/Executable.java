7594029784a822772e4b391014db9d16d2b8648d
Pia Joy G. Runes <pjgrunes@headoffice.corp.bpi.com.ph>
2025-12-05 18:33:30 +0800

    M2 Activity 4

---
+++ M2_act4/Executable.java
@@ -0,0 +1,9 @@
+package module2_activity4;
+
+public interface Executable {
+
+    void run();
+    void stop();
+
+}
+
---
+++ M2_act4/MSWord.java
@@ -0,0 +1,15 @@
+package module2_activity4;
+
+public class MSWord implements Executable {
+	
+	@Override
+	public void run() {
+		System.out.println("Opening MS Word...");
+	}
+	
+	@Override
+	public void stop() {
+		System.out.println("Stopping MS Word...");
+	}
+}
+
