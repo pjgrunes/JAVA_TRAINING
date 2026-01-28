99eafe8f4ca93c493cc282f0e4d18ecd647df5cc
Pia Joy G. Runes <pjgrunes@headoffice.corp.bpi.com.ph>
2026-01-28 14:18:41 +0800

    M6_Act1

---
+++ App.java
@@ -0,0 +1,24 @@
+package M6_Act1;
+
+import jakarta.persistence.EntityManager;
+
+public class App {
+	
+	   public static void main(String[] args) {
+	    	testConnection();
+	   }
+	   
+	   static void testConnection() {
+	    	EntityManager em = EntityManagerUtil.getInstance().createEntityManager();
+	    	
+	    	try {
+	    		if(em.isOpen()) {
+	    			System.out.println("entity manager open, ready to create transaction");
+	    		}
+	    		
+	    	} finally {
+	    		EntityManagerUtil.getInstance().closeEntityManager(em);
+	    		EntityManagerUtil.getInstance().shutdownFactory();
+	    	}
+	  }
+}
